c13ad6c699f1ba8a5088610eee15f361db6de59b
Enrique Olivares <enrique.olivares@wizeline.com>
2023-12-01 11:36:55 -0600

    feat: Created ETL and scan jobe

--- spc/hermes/jobs/__init__.py
+++ spc/hermes/jobs/__init__.py
@@ -1,15 +1,30 @@
-from dagster import job, op
+from dagster import AssetSelection, define_asset_job, job, op
+
+from spc.hermes.resources.soda import SodaResource
+
+EV_CHARGING_ETL = define_asset_job(
+    name="ev_charging_etl",
+    selection=AssetSelection.groups("ev_charging")
+    | (
+        AssetSelection.groups("ev_charging").downstream()
+        & AssetSelection.groups("analytics")
+    ),
+)
 
 
 @op
-def run_soda_scan():
-    pass
+def run_soda_scan(soda_: SodaResource):
+    scan = soda_.get_scan()
+    _ec = scan.execute()
+
+    if _ec != 0:
+        raise RuntimeError("Error during scan execution. Exit code is not 0.")
 
 
-@job()
-def taco():
+@job
+def ev_charging_scan():
     run_soda_scan()
 
 
 def get_jobs():
-    return [taco]
+    return [EV_CHARGING_ETL, ev_charging_scan]
